18e6e2f9f317959cd2a7cae580740bf5381a1a23
Harsha Nimmala <harsha0770@gmail.com>
2026-02-20 21:30:09 +0530

    feat: create multi module project

---
+++ src/main/java/com/flash/mapReduce/map/MapApplication.java
@@ -0,0 +1,11 @@
+package com.flash.mapReduce.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapApplication.class, args);
+    }
+}
---
+++ src/main/java/com/flash/mapReduce/reduce/ReduceApplication.java
@@ -0,0 +1,11 @@
+package com.flash.mapReduce.reduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReduceApplication.class, args);
+    }
+}
